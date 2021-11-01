a9afbebe628fc91a5c7d24d886605b4db6fa8b65
Rebecca Dearry <89046665+K2S00@users.noreply.github.com>
2021-10-31 22:43:38 -0400

    score and question tracking

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -79,6 +81,8 @@ getNewQuestion = () => {
         return window.location.assign('/end.html');
     }
     questionCounter++;
+  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
+    
     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
     currentQuestion = availableQuesions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -103,6 +107,9 @@ choices.forEach((choice) => {
         const classToApply =
         selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
         
+        if (classToApply === "correct") {
+            incrementScore(CORRECT_BONUS);
+        }
         selectedChoice.parentElement.classList.add(classToApply);
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
@@ -113,5 +120,9 @@ choices.forEach((choice) => {
         getNewQuestion();
     });
 });
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+  };
 
 startGame();
